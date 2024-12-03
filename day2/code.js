49f99d7c4cd747e0d8520f1bd1d1a35b9598f7d5
DMelisena <demonlordx777@gmail.com>
2024-12-03 15:00:12 +0700

    cleanup Codes from Tests
    dfsa

--- day2/code.js
+++ day2/code.js
@@ -1,76 +1,67 @@
 const fs = require('fs');
-
 let data = ""
 
-// Write data in 'Hello.txt' .
 fs.writeFile('passed.txt', data, (err) => {
-    // In case of a error throw err.
     if (err) throw err;
 })
 
 fs.writeFile('wrong.txt', data, (err) => {
-    // In case of a error throw err.
     if (err) throw err;
 })
 
 var text = fs.readFileSync('input.txt','utf8')//synchroneusly call
-//console.log (text)
 var array = text.split("\n")
 var narray = []
-//console.log(array)
 array.pop()
 
-// console.log(array)
 var narray = []
+
+function passFail(){
+
+}
 
 for (let i=0; i<array.length;i++){
   let line = array[i].split(" ");
   for (let l=0; l<line.length; l++){
     let currentNumber=line[l];
-  //  console.log(currentNumber)
     line[l]=parseInt(line[l]);
   }
-  // console.log(line)
   narray.push(line)
 }
 
-//console.log(narray)
-
-var length = 0
-var posit = true
+var length = 0;
+var posit = true;
+var failPass=true;
 
 var num = 0
-//console.log("num",num)
 for (let i=0; i<narray.length;i++){
   var pass = true;
   var even = false;
-  let current = narray[i][0]
-  var firstDis=narray[i][0]-narray[i][1]
+  let current = narray[i][0];
+  var firstDis=narray[i][0]-narray[i][1];
 
   if(firstDis<0){
-    posit = false 
+    posit = false;
   }
   else if(firstDis>0){
-    posit = true
+    posit = true;
   }
   else{
-    console.log("even",narray[i])
-    pass=false
+    console.log("even",narray[i]);
+    pass=false;
     continue;
   }
-  // console.log(posit,narray[i])
 
   for(let l=0; l<narray[i].length; l++){
 
-    length = narray[i][l]-narray[i][l+1]
+    length = narray[i][l]-narray[i][l+1];
   //  console.log(narray[i][l],narray[i][l+1])
 
     if (posit&&length<0){
       let arrayToString = narray[i].join(' ');
-      var wrongString = arrayToString
-      wrongString+="\n"
+      var wrongString = arrayToString;
+      wrongString+="\n";
       fs.appendFile('wrong.txt', wrongString, (err) => {
-          // In case of a error throw err.
           if (err) throw err;
       })
       pass = false;
@@ -78,10 +69,9 @@ for (let i=0; i<narray.length;i++){
     }
     else if (posit===false&&length>0){
       let arrayToString = narray[i].join(' ');
-      var wrongString = arrayToString
+      var wrongString = arrayToString;
       wrongString+="\n"
       fs.appendFile('wrong.txt', wrongString, (err) => {
-          // In case of a error throw err.
           if (err) throw err;
       })
       pass = false;
@@ -108,16 +98,11 @@ for (let i=0; i<narray.length;i++){
     }
   }
   if(pass){
-    //console.log("survivor",narray[i])
     let arrayToString = narray[i].join(' ');
-    //console.log("survivor",arrayToString)
     arrayToString+="\n"
-    // Data which will be appended to the file.
     num+=1
 
-    // Append data to 'Hello.txt' .
     fs.appendFile('passed.txt', arrayToString, (err) => {
-        // In case of a error throw err.
         if (err) throw err;
     })
   }
