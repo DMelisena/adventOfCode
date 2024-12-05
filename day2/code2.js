63369b2918dd9dfc2f5f6974c09f2dbdc95f94d2
DMelisena <demonlordx777@gmail.com>
2024-12-05 22:42:54 +0700

    Tidier code finished
    dsfas

--- day2/code2.js
+++ day2/code2.js
@@ -1,7 +1,6 @@
 const fs=require('fs');
 let data = ""
 
-
 //iniates File{{{
 fs.writeFile('passed2.txt', data, (err) => {
     if (err) throw err;
@@ -16,13 +15,11 @@ fs.writeFile('wrong2.txt', data, (err) => {
 function winNote(fileName,array){
   let arrayToString = array.join(' ');
   arrayToString+="\n"
-
   fs.appendFile(fileName, arrayToString, (err) => {
       if (err) throw err;
   })
 }
 //}}}
-
 
 //Turn Docs into array{{{
 var text = fs.readFileSync('input.txt','utf8')//synchroneusly call
@@ -40,24 +37,43 @@ for (let i=0; i<rawArray.length;i++){
 //}}}
 
 //initial parameter{{{
-var num=0
+let num = 0;
+let freePass = true;
+
 //}}}
 
-var positive = true;
+let positive = true;
 for (let i = 0; i<array.length; i++){
   // console.log(array[i]);
   let distance=array[i][1]-array[i][0]
   if(distance>0){
     positive = true;
-    num++
   }
   else if(distance<0){
     positive = false;
-    num++
   }
   else{
     continue;
   }
-  // console.log(positive,distance,array[i][0],array[i][1])
- 
+  console.log(positive,distance,array[i][0],array[i][1],array[i],array[i].length)
+
+  for (let l=0; l<array[i].length-1;l++){
+    distance=array[i][l+1]-array[i][l];
+    // console.log(positive,distance,array[i])
+    if(positive&&distance>0&&distance<4){
+    }
+    else if(positive===false&&distance<0&&distance>-4){
+      distance =-distance;
+    }
+    else{
+      break;
+    }
+
+    if(l===array[i].length-2){
+      console.log('finished',l)
+      winNote('passed2.txt',array[i]);
+      num++
+    }
+  }
 }
+console.log(num)
